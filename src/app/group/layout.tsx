0d62cf839bc25fd1199033297e6bb47aca0efdfd
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2026-06-01 20:15:38 +0500

    fix layout naming

--- src/app/group/layout.tsx
+++ src/app/group/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 import { buttonVariants } from "@/components/ui/button";
 
-export default function AuthLayout({ children }: { children: ReactNode }) {
+export default function GroupLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="absolute top-5 left-5">
