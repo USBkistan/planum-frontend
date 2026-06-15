f17091f57de0c94779787ad9d2beb33965ccc8d1
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2026-06-15 18:40:28 +0500

    add padding

--- src/components/web/comments.tsx
+++ src/components/web/comments.tsx
@@ -197,7 +197,7 @@ export default function TaskComments({ taskId, user }: { taskId: string; user: U
   return (
     <>
       <Card>
-        <CardTitle className="mb-4 flex items-center gap-2 text-lg font-semibold">
+        <CardTitle className="mb-4 flex items-center gap-2 px-4 text-lg font-semibold">
           <MessageSquare className="h-5 w-5" />
           Комментарии
         </CardTitle>
